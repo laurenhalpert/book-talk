b419a1dff4a230857f5729e4cb8881aa3db097b7
Lauren <halpertln@gmail.com>
2023-08-12 09:21:22 -0600

    add comments for todos

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -9,7 +9,6 @@ function HomePage(props) {
                 <h1>Book Talk</h1>
             </header>
             <p>Welcome to Book Talk! A place for book lovers to socialize with others who love the same book. Log in, or sign up to get started!</p>
-            <img src="https://www.gandydancer.org/wp-content/uploads/2016/02/blog-image.jpg" alt="book"></img>
             <div>
                 <LogIn />
             </div>
--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -2,20 +2,24 @@ import React, { useEffect, useState } from "react";
 
 
 function LogIn(props) {
+    
     return (
-        <div>
+        <div id="logInForm">
             <form>
                 <label htmlFor="usernameField">Username: </label>
-                <input id="usernameField" type="text"></input>
+                <input id="usernameField" type="text" placeholder="Username..."></input>
                 <br></br>
                 <label htmlFor="passwordField">Password: </label>
-                <input id="passwordField" type="text"></input>
+                <input id="passwordField" type="text" placeholder="Password..."></input>
                 <br></br>
-                <input id="submitButton" type="submit">Log In</input>
+                <button id="submitBtn" type="submit">Log In</button>
+                {/* set up routing so that when "Log In" is clicked the username and password are authenticated and the user is redirected to their user homepage if authenticated, if not they receive an error message */}
             </form>
             <p>Don't have an account? Sign up below.</p>
             <button>Sign Up</button>
+            {/* set up routing so that when "Sign Up" is clicked, the user is routed to the Sign Up page */}
         </div>
+
     )
     
 }
