3b206fb8d9100d46e109407ff3adf403c64b251b
Lauren <halpertln@gmail.com>
2023-08-18 08:22:06 -0600

    edit book component

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 function Book({ book }) {
     return (
         <div>
+            <img className="bookCover" src={book.book_image} alt="book cover"></img>
             <h2>{book.title}</h2>
             <h3>By: {book.author_first_name} {book.author_last_name}</h3>
         </div>
