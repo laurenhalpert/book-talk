cd393dedc7f88fb6f8b3ebb1cf3ba34aea1555be
Lauren <halpertln@gmail.com>
2023-09-12 13:46:07 -0600

    newpost form not working

--- client/src/components/NewPostForm.js
+++ client/src/components/NewPostForm.js
@@ -1,48 +1,113 @@
 import React, { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
 // NEEDS formik
 function NewPostForm({ user, book, onAddPost, posts }) {
-    const [formData, setFormData] = useState({
-        user_id: user.id,
-        book_id: book.id,
-        likes: 0,
-        post_content: ""
-    })
-    
-    const handleChange = e => {
-        console.log(e.target.name)
-        setFormData({
-            ...formData,
-            post_content: e.target.value,
-            likes: 0
-        })
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault()
-        fetch(`/book_index/${book.id}`, {
+    // const [formData, setFormData] = useState({
+    //     user_id: user.id,
+    //     book_id: book.id,
+    //     likes: 0,
+    //     post_content: ""
+    // })
+    const formSchema = yup.object().shape({
+        post_content: yup.string().required("Must enter content"),
+       
+    });
+    const formik = useFormik({
+        initialValues: {
+            post_content: "",
+            
+        },
+        // change fetch URL
+        // update html with formik.values and formik.handles
+        validationSchema: formSchema,
+        onSubmit: (values) => {
+          fetch(`/book_index/${book.id}`, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+              "Content-Type": "application/json",
             },
-            body: JSON.stringify(formData)
-        })
-        .then (r => r.json())
-        .then (newPost => {
-            onAddPost([...posts, newPost]);
-            setFormData({
-                ...formData,
-                post_content: ""
-            })
-        })
+            body: JSON.stringify(values, null, 2),
+          }).then((res) => {
+            if (res.status == 201) {
+                console.log(res);
+                console.log(values)
+                
+                // Why key error?
+                
+
+
+            //   setRefreshPage(!refreshPage)
+            }
+          });
+        },
+    });
+    // const formSchema = yup.object().shape({
+        
+    //     post_content: yup.string().required("Must enter content"),
+    // });
+    // const formik = useFormik({
+    //     initialValues: {
+            
+    //         post_content: ""
+    //     },
+    //     validationSchema: formSchema,
+    //     onSubmit: (values) => {
+    //       fetch(`/book_index/${book.id}`, {
+    //         method: "POST",
+    //         headers: {
+    //           "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify(values, null, 2),
+    //       }).then((res) => {
+    //         if (res.status == 201) {
+    //             console.log(res);
+    //             console.log(values)
+                
+                
+                
+
+
+    //         //   setRefreshPage(!refreshPage)
+    //         }
+    //       });
+    //     },
+    // });
+    // const handleChange = e => {
+    //     console.log(e.target.name)
+    //     setFormData({
+    //         ...formData,
+    //         post_content: e.target.value,
+    //         likes: 0
+    //     })
+    // }
+
+    // function handleSubmit(e) {
+    //     e.preventDefault()
+    //     fetch(`/book_index/${book.id}`, {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify(formData)
+    //     })
+    //     .then (r => r.json())
+    //     .then (newPost => {
+    //         onAddPost([...posts, newPost]);
+    //         setFormData({
+    //             ...formData,
+    //             post_content: ""
+    //         })
+    //     })
 
         // .then set posts to add in new post
 
-    }
+    
     return(
         <div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor="newPost">New Post: </label>
-                <textarea id="newPost" name="newPost" placeholder="Thoughts..." onChange={handleChange} value={formData.post_content}></textarea>
+                <textarea id="newPost" name="newPost" placeholder="Thoughts..." value={formik.values.post_content} onChange={formik.handleChange} ></textarea>
                 <button id="submitBtn" type="submit" >Post</button>
             </form>
         </div>
