ea8cb3594c2722a24e74ad2852fb0e477676a67e
Lauren <halpertln@gmail.com>
2023-09-22 08:42:51 -0600

    delete console logs and comments newpostform.js

--- client/src/components/NewPostForm.js
+++ client/src/components/NewPostForm.js
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
-// NEEDS formik
+
 function NewPostForm({ user, book, onAddPost, posts }) {
-    // const [formData, setFormData] = useState({
-    //     user_id: user.id,
-    //     book_id: book.id,
-    //     likes: 0,
-    //     post_content: ""
-    // })
+   
 
 
     const formSchema = yup.object().shape({
@@ -22,8 +17,7 @@ function NewPostForm({ user, book, onAddPost, posts }) {
             user_id: user.id,
             book_id: book.id,
         },
-        // change fetch URL
-        // update html with formik.values and formik.handles
+        
         validationSchema: formSchema,
         onSubmit: (values) => {
           fetch(`/book_index/${book.id}`, {
@@ -34,21 +28,14 @@ function NewPostForm({ user, book, onAddPost, posts }) {
             body: JSON.stringify(values, null, 2),
           }).then((res) => {
             if (res.status == 201) {
-                console.log(res);
-                console.log(values)
+                
                 fetch(`/book_index/${book.id}`)
                 .then(r=>r.json())
                 .then(posts => {
                 onAddPost(posts)
                 
                 })
-                // onAddPost([...posts, values])
-                // Why key error?
-                // fetch again fresh posts for thisBook
                 
-
-
-            //   setRefreshPage(!refreshPage)
             }
           });
         },
@@ -60,93 +47,7 @@ function NewPostForm({ user, book, onAddPost, posts }) {
 
 
 
-    // const formSchema = yup.object().shape({
-    //     post_content: yup.string().required("Must enter content"),
-       
-    // });
-    // const formik = useFormik({
-    //     initialValues: {
-    //         post_content: "",
-            
-    //     },
-    //     // change fetch URL
-    //     // update html with formik.values and formik.handles
-    //     validationSchema: formSchema,
-    //     onSubmit: (values) => {
-    //       fetch(`/book_index/${book.id}`, {
-    //         method: "POST",
-    //         headers: {
-    //           "Content-Type": "application/json",
-    //         },
-    //         body: JSON.stringify(values, null, 2),
-    //       }).then((res) => {
-    //         if (res.status == 201) {
-    //             console.log(res);
-    //             console.log(values)
-    //         }
-    //       });
-    //     },
-    // });
-    // const formSchema = yup.object().shape({
-        
-    //     post_content: yup.string().required("Must enter content"),
-    // });
-    // const formik = useFormik({
-    //     initialValues: {
-            
-    //         post_content: ""
-    //     },
-    //     validationSchema: formSchema,
-    //     onSubmit: (values) => {
-    //       fetch(`/book_index/${book.id}`, {
-    //         method: "POST",
-    //         headers: {
-    //           "Content-Type": "application/json",
-    //         },
-    //         body: JSON.stringify(values, null, 2),
-    //       }).then((res) => {
-    //         if (res.status == 201) {
-    //             console.log(res);
-    //             console.log(values)
-                
-                
-                
-
-
-    //         //   setRefreshPage(!refreshPage)
-    //         }
-    //       });
-    //     },
-    // });
-    // const handleChange = e => {
-    //     console.log(e.target.name)
-    //     setFormData({
-    //         ...formData,
-    //         post_content: e.target.value,
-    //         likes: 0
-    //     })
-    // }
-
-    // function handleSubmit(e) {
-    //     e.preventDefault()
-    //     fetch(`/book_index/${book.id}`, {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json"
-    //         },
-    //         body: JSON.stringify(formData)
-    //     })
-    //     .then (r => r.json())
-    //     .then (newPost => {
-    //         onAddPost([...posts, newPost]);
-    //         setFormData({
-    //             ...formData,
-    //             post_content: ""
-    //         })
-    //     })
-
-        // .then set posts to add in new post
-
+   
     
     return(
         <div>
