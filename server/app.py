d66fea64c1c21b9e47b28e7a43983aa455d77686
Lauren <halpertln@gmail.com>
2023-09-22 08:54:12 -0600

    delete print statements and comments app.py

--- server/app.py
+++ server/app.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python3
 
-# Standard library imports
 
-# Remote library imports
 from flask import request, session, make_response, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 import sys
 import pandas as pd
-# Local imports
+
 from config import app, db, api
 from models import Book, User, Post, MyBook
 
 
-# Views go here!
+
 class SignUp(Resource):
     def post(self):
-        print(session)
+        
         username = request.get_json()['username']
         password = request.get_json()['password']
         image_url = request.get_json()['image_url']
         bio = request.get_json()['bio']
-        # username_check = User.query.filter(User.username == username).first()
-        # print(username_check)
-        # print(not username_check)
+        
         if username:
             try:
                 new_user = User(username=username, image_url=image_url, bio=bio)
@@ -35,17 +31,14 @@ class SignUp(Resource):
                 session['user_id'] = new_user.id
                 return new_user.to_dict(), 201
             except: 
-                print('username not unique')
+                
                 return {"error": "username not unique"}, 422
-        # elif username and (username_check == False):
-        #     alert('This username is already taken')
-        #     return {"error": "422 Unprocessable entity"}
-        # handle someone trying to sign up with a username that already exists
+        
         return {"error": "422 Unprocessable entity"}, 422 
 
 class CheckSession(Resource):
     def get(self):
-        print(session)
+        
         id = session.get('user_id')
         user = User.query.filter(User.id == id).first()
         if session.get('user_id'):
@@ -54,11 +47,10 @@ class CheckSession(Resource):
 
 class LogIn(Resource):
     def post(self):
-        print(session)
+        
         username= request.get_json()['username']
         password = request.get_json()['password']
-        # image_url = request.get_json()['image_url']
-        # bio = request.get_json()['bio']
+        
 
         user = User.query.filter_by(username =username).first()
 
@@ -68,39 +60,34 @@ class LogIn(Resource):
                 return user.to_dict(), 200
         return {"error": "401 unauthorized"}, 401
     def get(self):
-        print(session)
+        
         user = User.query.filter_by(id=session['user_id']).first()
         return user.to_dict(), 200
 
 
 class LogOut(Resource):
     def delete(self):
-        print(session)
         
-        # print(user)
         id = session.get('user_id')
         user = User.query.filter_by(id = id).first()
-        print(user)
+        
         if user:
-            print('into this section')
+            
             session['user_id'] = None
-            print(session)
+            
             return {}, 204
-            print(session)
-        # else:
-        #     return {'error': '401 Unauthorized'}
+   
 
 class MyBookIndex(Resource):
     def get (self):
-        print(session)
+        
         if session.get('user_id'):
             user = User.query.filter(User.id == session['user_id']).first()
-            print(user.books)
+            
             return [book.to_dict() for book in user.books], 200
         return {'error': '401 Unauthorized'}, 401
     def post (self):
-        # if session.get('user.id'):
-        print(session)
+        
         request_json = request.get_json()
         book_id=request.get_json()["book_id"]
         user_id=request.get_json()["user_id"]
@@ -114,15 +101,15 @@ class MyBookIndex(Resource):
         return new_my_book.to_dict(), 201
 class ThisMyBook(Resource):
     def get (self, my_id):
-        print(session)
+        
         book = Book.query.filter(Book.id == my_id).first()
         return book.to_dict(), 200
     def get (self, my_id):
-        print(session)
+        
         posts = Post.query.filter(Post.book_id == my_id).all()
         return [post.to_dict() for post in posts], 200
     def post (self, my_id):
-        print(session.get('user_id'))
+       
         if session.get('user_id'):
             request_json = request.get_json()
             post_content = request_json['post_content']
@@ -136,72 +123,48 @@ class ThisMyBook(Resource):
                 return {'error': '422 Unprocessable Entity'}, 422
         return {'error': '401 Unauthorized'}, 401
     def delete (self, my_id):
-        print(session)
+        
         book = MyBook.query.filter_by(book_id = my_id, user_id = session.get('user_id')).first()
-        # breakpoint()
+        
         db.session.delete(book)
         db.session.commit()
         return {}, 204
-        # if session.get('user_id'):
-        #     request_json = request.get_json()
-        #     print(my_id)
-        #     book = MyBook.query.filter(MyBook.book_id == my_id).first()
-        #     print(book)
-        #     db.session.delete(book)
-        #     db.session.commit()
-        #     return {}, 204
-        # else :
-        #     return {'error': '401 Unathorized'}, 401
+        
 
 
 class ThisMyBookPost(Resource):
     def patch(self, my_id, my_post_id):
-        print(session)
+        
         
         post = Post.query.filter(Post.id == my_post_id).first()
-        print(type(post.likes))
-        print(post)
+        
         post.likes += 1
         
-        # for attr in request.form:
-        #     print(attr)
-        #     setattr(post, attr, request.form[attr])
+       
         db.session.add(post)
         db.session.commit()
 
         return post.to_dict(), 201
     def delete(self, my_id, my_post_id):
-        print(session)
+       
         post = Post.query.filter(Post.id == my_post_id).first()
 
         db.session.delete(post)
         db.session.commit()
         return {}, 204
-        # return {'error': '401 Unauthorized'}, 401
+        
 
 class BookIndex(Resource):
     def get(self):
-        print(session)
+        
         books = Book.query.all()
-        print(books)
-        # print(f"{type(book)=}")
-        # print(f"{book=} ")
-        # # sys.exit(1) 
-        # print(f"{dir(book)=}") 
-        # print(f"{book.__dict__=}")
-        # return [{"title": book.title} for book in books], 200
+      
         
         return [book.to_dict() for book in books], 200
         
-        # ['<Book: law>', '<Book: order>']
-        # [
-        #     {
-        #         'title': 'law',
-        #         'author_first_name': 'Lauren'
-        #     }
-        # ]
+        
     def post(self):
-        print(session)
+        
         title= request.get_json()['title']
         author_first_name = request.get_json()['author_first_name']
         author_last_name = request.get_json()['author_last_name']
@@ -225,15 +188,15 @@ class BookIndex(Resource):
 
 class ThisBook(Resource):
     def get (self, id):
-        print(session)
+        
         book = Book.query.filter(Book.id == id).first()
         return book.to_dict(), 200
     def get (self, id):
-        print(session)
+        
         posts = Post.query.filter(Post.book_id == id).all()
         return [post.to_dict() for post in posts], 200
     def post (self, id):
-        print(session.get('user_id'))
+        
         if session.get('user_id'):
             request_json = request.get_json()
             post_content = request_json['post_content']
@@ -248,22 +211,18 @@ class ThisBook(Resource):
         return {'error': '401 Unauthorized'}, 401
 class ThisBookPost (Resource):
     def patch(self, id, post_id):
-        print(session)
         
         post = Post.query.filter(Post.id == post_id).first()
-        print(type(post.likes))
-        print(post)
+        
         post.likes += 1
         
-        # for attr in request.form:
-        #     print(attr)
-        #     setattr(post, attr, request.form[attr])
+       
         db.session.add(post)
         db.session.commit()
 
         return post.to_dict(), 201
     def delete(self, id, post_id):
-        print(session)
+        
         post = Post.query.filter(Post.id == post_id).first()
 
         db.session.delete(post)
@@ -286,9 +245,3 @@ api.add_resource(ThisBookPost, '/api/book_index/<int:id>/<int:post_id>', endpoin
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
 
-
-
-# check session seems to not be working properly...bc of state? or something else?
-# add in create book to BookIndex
-
-# check on other fetch calls to /mbi/id to see if they work
