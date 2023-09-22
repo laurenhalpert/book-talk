7dae0b977291f2131b6bd178fe16f236d698fe9e
Lauren <halpertln@gmail.com>
2023-09-22 08:56:23 -0600

    delete comments models.py

--- server/models.py
+++ server/models.py
@@ -3,16 +3,10 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import validates, relationship, backref, sessionmaker
 from config import db, bcrypt
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
+
 from sqlalchemy.ext.associationproxy import association_proxy
 
-# engine = create_engine('sqlite:///book_talk.db')
-# Session = sessionmaker(bind=engine)
-# session = Session()
 
-
-# Base = declarative_base()
 
 
 class Book (db.Model, SerializerMixin):
@@ -33,16 +27,7 @@ class Book (db.Model, SerializerMixin):
     users = association_proxy('posts', 'user', creator=lambda ur: Post(user=ur))
     
 
-    # def to_dict(self):
-    #     return {
-    #         "id": self.id,
-    #         "title": self.title,
-    #         "author_first_name": self.author_first_name,
-    #         "author_last_name": self.author_last_name,
-    #         "genre": self.genre,
-    #         "book_image": self.book_image,
-    #         "description": self.description
-    #     }
+    
     def __repr__(self):
         return f'<Book: {self.title} >'
 
@@ -57,13 +42,7 @@ class Post (db.Model, SerializerMixin):
 
     
 
-    # def to_dict(self):
-    #     return {
-    #         "id": self.id,
-    #         "post_content": self.post_content,
-    #         "user_id": self.user_id,
-    #         "book_id": self.book_id
-    #     }
+
     
     def __repr__(self):
         return f'<Post: {self.id} Likes: {self.likes}>'
@@ -71,7 +50,7 @@ class Post (db.Model, SerializerMixin):
 class User (db.Model, SerializerMixin):
     __tablename__ = 'users'
     serialize_rules = ('-_password_hash', 'books')
-    # serialize_rules = ('-books.user', '-user.books', '-posts.book', '-posts.user')
+    
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String, unique = True, nullable=False)
     _password_hash = db.Column(db.String)
@@ -82,16 +61,7 @@ class User (db.Model, SerializerMixin):
     posts = db.relationship('Post', backref='user')
     my_books = db.relationship('MyBook', backref='user')
     books = association_proxy('posts', 'book', creator = lambda bk: Post(book = bk))
-    # right now dog.books is nothing because books is related through posts...maybe I can relate through MyBooks? That'd also require me to properly set up the MyBook relationship
-
-    # @validates('username')
-    # def validate_user(self, username):
-    #     if not User.query.filter_by('username'==username).first():
-    #         raise ValueError('Username does not exist. Please enter a valid username or create an account')
-    #     elif not username:
-    #         raise ValueError('Please enter a username or create and account.')
-    #     else:
-    #         return username
+    
     @hybrid_property
     def password_hash(self):
         raise AttributeError('Password hashes may not be viewed.')
@@ -116,14 +86,8 @@ class MyBook(db.Model, SerializerMixin):
     book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    # def to_dict(self):
-    #     return {
-    #         "id": self.id,
-    #         "book_id": self.book_id,
-    #         "user_id": self.user_id,
-    #     }
+   
 
     def __repr__(self):
         return f'<MyBook {self.id}>'
 
-# try different serialize rules to stop the looping
